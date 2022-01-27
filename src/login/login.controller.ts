161a0ac53f1e8c5a5b342e5ad055e185c74f6299
Leesungkyun <g1plinc@gmail.com>
2022-01-27 11:14:55 +0900

    두번째 커밋

---
+++ src/login/login.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Get, Post, Query, Redirect, Res } from '@nestjs/common';
+import { LoginService } from './login.service';
+import * as kakao_auth from '../config/kakao_auth';
+import { Response } from 'express';
+import { URLSearchParams } from 'url';
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('카카오 인증')
+@Controller('login')
+export class LoginController {
+    constructor(private readonly loginService: LoginService) { }
+    
+    @Get('callbacks/kakao/sign_in')
+    @Redirect('', 307)
+    getKakao(@Query() query) {
+        return { url: `webauthcallback://success?${new URLSearchParams(query).toString()}` };
+    }
+
+    @Post('callbacks/kakao/token')
+    getKakaoToken(@Body() body, @Res() response: Response) {
+        kakao_auth.createFirebaseToken(body["accessToken"], (results) => {
+            response.send(results);
+        });
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Plinic2-Backend')
+    .setDescription('플리닉 백엔드 API정의')
+    .setVersion('0.1')
+    .addTag('Plinic2')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8001);
 }
 bootstrap();
