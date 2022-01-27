202511ed1bd2df7c847963ce81d076a2e36b1349
Leesungkyun <g1plinc@gmail.com>
2022-01-27 12:24:57 +0900

    스웨거 레지스터 apitags 추가

--- src/register/register.controller.ts
+++ src/register/register.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { RegisterService } from './register.service';
 import { CreateRegisterDto } from './dto/create-register.dto';
 import { UpdateRegisterDto } from './dto/update-register.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('SNS인증 후 Monodg 회원정보저장 (회원정보, 토큰정보, 약관동의정보, 알림유무')
 @Controller('register')
 export class RegisterController {
   constructor(private readonly registerService: RegisterService) {}
