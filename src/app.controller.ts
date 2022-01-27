f6ee666d537779ca0da1bc71565e48a86858a61c
Leesungkyun <g1plinc@gmail.com>
2022-01-27 12:11:24 +0900

    SNS로그인 후 회원가입 모듈 최초 등록

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags('SNS인증 후 회원가입')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoginModule } from './login/login.module';
+import { RegisterModule } from './register/register.module';
 
 @Module({
-  imports: [LoginModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://plinic:plinic0608@plinic2.rdvo6.mongodb.net/plinic2?retryWrites=true'),
+    LoginModule,
+    RegisterModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
