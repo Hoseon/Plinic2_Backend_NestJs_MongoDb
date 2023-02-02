f03f0b302ad748a3e0efdf9d6e28991e0e59cf2b
hoseon <chs1025@gmail.com>
2023-02-02 16:35:59 +0900

    2023-02-02 이관완료

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,8 @@ async function bootstrap() {
     .addTag('Plinic2')
     .build();
   
+
+  
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.use(express.json({ limit: '50mb' }));
